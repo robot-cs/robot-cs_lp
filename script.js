03f431c23679ef71f330a3a6cf2c0711d288a7f8
Robot Creators <119015140+robot-cs@users.noreply.github.com>
2022-12-30 18:34:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ video_btn.addEventListener('click',() => {
 		is_playing=false;
 	}
 });
+
+(window.onload = function() {
+
+  // フェードイン処理
+  jQuery(window).scroll(function (){
+    jQuery(".fade").each(function(){
+      var winheight = jQuery(window).height();
+      var posi = jQuery(this).offset().top;
+      var scroll = jQuery(window).scrollTop();
+      if (scroll + winheight > posi){
+        jQuery(this).addClass("fadein");
+      } else {
+        //　スクロールで画面上部に戻った際に要素を非表示にしたい場合は、下記の行のコメントを外し有効にしてください。
+        //jQuery(this).removeClass("fadein");
+      }
+    });
+   });
+
+})(); 
