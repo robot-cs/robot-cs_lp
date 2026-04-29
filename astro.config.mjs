85ac2d94d4ec81e94e340b73060a27ec00581b14
mokuzai-cell <7525109@ed.tus.ac.jp>
2026-04-29 17:32:45 +0900

    全部base基準に設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import vue from "@astrojs/vue";
 
 export default defineConfig({
   // GitHub Pagesでリポジトリ名がURLに入る場合に必要
-  site: "https://robot-cs.github.io/robot-cs_lp/",
-  base:"",
+  site: "https://robot-cs.github.io",
+  base:"/robot-cs_lp",
 
 
   integrations: [vue()],
