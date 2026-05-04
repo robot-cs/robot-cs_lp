8a1065311c69de2517bbf198e8b28f433c71dfd8
mokuzai-cell <7525109@ed.tus.ac.jp>
2026-05-04 20:29:30 +0900

    修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 
 import vue from "@astrojs/vue";
 
+import mdx from "@astrojs/mdx";
+
 export default defineConfig({
   // GitHub Pagesでリポジトリ名がURLに入る場合に必要
   site: "https://robot-cs.github.io",
   base:"/robot-cs_lp",
 
 
-  integrations: [vue()],
+  integrations: [vue(), mdx()],
 });
