2a32b53fe5eb99678ec77339635e3dac035390d0
mokuzai-cell <7525109@ed.tus.ac.jp>
2026-05-17 10:42:09 +0900

    add blog 3

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,7 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_Z143K83 from "../../assets/blogImg/1778979033989.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F3.md";
+import __ASTRO_IMAGE_IMPORT_eWdSF from "../../assets/blogImg/1778979038620.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F3.md";
+import __ASTRO_IMAGE_IMPORT_ILfHb from "../../assets/blogImg/1778979038682.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F3.md";
+import __ASTRO_IMAGE_IMPORT_7hvq1 from "../../assets/blogImg/1778979040313.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F3.md";
+export default new Map([["../../assets/blogImg/1778979033989.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F3.md", __ASTRO_IMAGE_IMPORT_Z143K83], ["../../assets/blogImg/1778979038620.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F3.md", __ASTRO_IMAGE_IMPORT_eWdSF], ["../../assets/blogImg/1778979038682.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F3.md", __ASTRO_IMAGE_IMPORT_ILfHb], ["../../assets/blogImg/1778979040313.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F3.md", __ASTRO_IMAGE_IMPORT_7hvq1]]);
+		
